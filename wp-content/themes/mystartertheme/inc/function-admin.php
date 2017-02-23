b6ea41dc28cf4a6cafb83a0cb9f57ece63fa5fba
cannelflow <mightyrocket@hotmail.com>
2017-02-23 19:37:40 +0000

    Lession 3

--- wp-content/themes/mystartertheme/inc/function-admin.php
+++ wp-content/themes/mystartertheme/inc/function-admin.php
@@ -11,11 +11,22 @@
 
 function mysunset_add_admin_page() {
 	
-	add_menu_page( 'Sunset Theme Options', 'Sunset', 'manage_options', 'alecaddd-sunset', 'mysunset_theme_create_page', get_template_directory_uri() . '/img/sunset-icon.png', 110 );
+	//Generate Sunset Admin Page
+	add_menu_page( 'Sunset Theme Options', 'Sunset', 'manage_options', 'cannelflow_sunset', 'mysunset_theme_create_page', get_template_directory_uri() . '/img/sunset-icon.png', 110 );
+	
+	//Generate Sunset Admin Sub Pages
+	add_submenu_page( 'cannelflow_sunset', 'Sunset Theme Options', 'General', 'manage_options', 'cannelflow_sunset', 'mysunset_theme_create_page' );
+	add_submenu_page( 'cannelflow_sunset', 'Sunset CSS Options', 'Custom CSS', 'manage_options', 'cannelflow_sunset_css', 'mysunset_theme_settings_page');
 	
 }
 add_action( 'admin_menu', 'mysunset_add_admin_page' );
 
 function mysunset_theme_create_page() {
 	//generation of our admin page
+	require_once( get_template_directory() . '/inc/templates/sunset-admin.php' );
+}
+
+function mysunset_theme_settings_page() {
+	//generation of our admin page
+	echo '<h1>Sunset Custom CSS</h1>';
 }
